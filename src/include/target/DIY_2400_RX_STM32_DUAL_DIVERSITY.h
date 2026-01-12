bfe32fc736551e2814e91d014b2dbab9f179be97
Dominic Clifton <me@dominicclifton.name>
2026-01-12 19:18:21 +0100

    DIY_2400_TX_DUAL_DIVERSITY target.

---
+++ src/include/target/DIY_2400_RX_STM32_DUAL_DIVERSITY.h
@@ -0,0 +1,55 @@
+#ifndef DEVICE_NAME
+#define DEVICE_NAME "ELRS 2400RX DD"
+#endif
+
+// Pins ordered by physical layout of UFQFN32, do not re-order.
+
+// PA0, PA1, PA2, PA3 = flexible.
+// PA0 = ADC1 IN5, TIM2_CH1
+// PA1 = ADC1 IN6, TIM2_CH2
+// PA2 = ADC1 IN7, TIM2_CH3, UART2_RX
+// PA3 = ADC1 IN8, TIM2_CH4, UART2_TX
+#define GPIO_PIN_DEBUG_RX PA3
+#define GPIO_PIN_DEBUG_TX PA2
+
+#define GPIO_PIN_NSS         PA4
+
+#define GPIO_PIN_SCK         PA5
+#define GPIO_PIN_MISO        PA6
+#define GPIO_PIN_MOSI        PA7
+
+// EXTI0
+#define GPIO_PIN_DIO1        PB0
+// EXTI1
+#define GPIO_PIN_DIO1_2      PB1
+
+#define GPIO_PIN_LED_WS2812         PA8
+#define GPIO_PIN_LED_WS2812_FAST    PA_8
+
+// UART1 on default pins
+#define GPIO_PIN_RCSIGNAL_RX PA9
+#define GPIO_PIN_RCSIGNAL_TX PA10
+
+// PA11 + PA12 can be used for FDCAN or USB
+// PA13 + PA14 + PB3 for SWDIO, SWCLK, SWO
+
+#define GPIO_PIN_NSS_2       PA15
+
+#define GPIO_PIN_RST         PB4
+#define GPIO_PIN_RST_2       PB5
+
+// Use a single-pin per PA, check truth table of PA - RFX2401C Control Logic is suitable if RXEN has a pull-up.
+// NOTE: On the NucleoL432 SB16 and SB18 must be removed to use these pins!
+// or could use PB6 and PB7 as I2C SCL/SDA (respectively) when not using a Power Amplifiers
+#define GPIO_PIN_TX_ENABLE   PB6
+#define GPIO_PIN_TX_ENABLE_2 PB7
+
+#define GPIO_PIN_BUTTON      PH3
+
+// EXTI14
+#define GPIO_PIN_BUSY        PC14
+// EXTI15
+#define GPIO_PIN_BUSY_2      PC15
+
+// Output Power - use default SX1280
+#define POWER_OUTPUT_FIXED 13 //MAX power for 2400 RXes that doesn't have PA is 12.5dbm
