e8f10506d3dcd99fb851fadb0dcb2e33c34f0413
Dominic Clifton <me@dominicclifton.name>
2026-01-13 22:59:25 +0100

    Disable some settings to try and get it working with one SX1280 first.

--- src/include/target/DIY_2400_RX_STM32_DUAL_DIVERSITY.h
+++ src/include/target/DIY_2400_RX_STM32_DUAL_DIVERSITY.h
@@ -11,8 +11,8 @@
 // PA1 = ADC1 IN6, TIM2_CH2
 // PA2 = ADC1 IN7, TIM2_CH3, UART2_RX
 // PA3 = ADC1 IN8, TIM2_CH4, UART2_TX
-#define GPIO_PIN_DEBUG_RX           PA3
-#define GPIO_PIN_DEBUG_TX           PA2
+//#define GPIO_PIN_DEBUG_RX           PA3
+//#define GPIO_PIN_DEBUG_TX           PA2
 
 #define GPIO_PIN_NSS                PA4
 
@@ -23,14 +23,14 @@
 // EXTI0
 #define GPIO_PIN_DIO1               PB0
 // EXTI1
-#define GPIO_PIN_DIO1_2             PB1
+//#define GPIO_PIN_DIO1_2             PB1
 
-#define GPIO_PIN_LED_WS2812         PA8
-#define GPIO_PIN_LED_WS2812_FAST    PA_8
+//#define GPIO_PIN_LED_WS2812         PA8
+//#define GPIO_PIN_LED_WS2812_FAST    PA_8
 
 // UART1 on default pins
-#define GPIO_PIN_RCSIGNAL_RX        PA9
-#define GPIO_PIN_RCSIGNAL_TX        PA10
+#define GPIO_PIN_RCSIGNAL_TX        PA9
+#define GPIO_PIN_RCSIGNAL_RX        PA10
 
 #define SERIAL_UART_INSTANCE        1
 #define PIN_SERIAL_RX               PA9
@@ -44,25 +44,25 @@
 
 // On the NucleoL432 this involves removing SB2 and soldering a wire to the pad that connects to the L4 MCU
 // as it is not available on a header pin or connector.
-#define GPIO_PIN_NSS_2              PA15
+//#define GPIO_PIN_NSS_2              PA15
 
 // Using seperate RST signals to avoid a known issue when the NRESET signals of two SX1280 are connected to each other.
 // To free a pin if needed a 1k + 100nf LC filter can be put in front of each of the SX1280's NRESET pins and then the 
 // RST net should contain the L432 + 1k + 1k
 #define GPIO_PIN_RST                PB4
-#define GPIO_PIN_RST_2              PB5
+//#define GPIO_PIN_RST_2              PB5
 
 // Use a single-pin per PA, check truth table of PA - RFX2401C Control Logic is suitable if RXEN has a pull-up.
 // NOTE: On the NucleoL432 SB16 and SB18 must be removed to use these pins!
 // or could use PB6 and PB7 as I2C SCL/SDA (respectively) when not using a Power Amplifiers
-#define GPIO_PIN_TX_ENABLE          PB6
-#define GPIO_PIN_TX_ENABLE_2        PB7
+//#define GPIO_PIN_TX_ENABLE          PB6
+//#define GPIO_PIN_TX_ENABLE_2        PB7
 
 // PH3 can be used for a button, but there's compilation errors currently.
 //#define GPIO_PIN_BUTTON             PH3
 
 // EXTI14 (NucleoL432 D8 with SB6 installed, SB5 removed, SB4 removed)
-#define GPIO_PIN_BUSY_2             PC14
+//#define GPIO_PIN_BUSY_2             PC14
 // EXTI15 (NucleoL432 D7 with SB8 installed, SB7 removed)
 #define GPIO_PIN_BUSY               PC15
 
