75f047e585c453d2a45fa7e9b6b772f19d2fc0d4
Dominic Clifton <me@dominicclifton.name>
2026-04-01 17:21:05 +0200

    Add logging when DCDC is enabled to the LR1121 and SX1280 drivers.
    * extremely helpful when debugging a target failing due to incorrect hardware configuration and to confirm that DCDC is being enabled as expected.

--- src/lib/SX1280Driver/SX1280.cpp
+++ src/lib/SX1280Driver/SX1280.cpp
@@ -136,6 +136,7 @@ transitioning from FS mode and the other from Standby mode. This causes the tx d
 #if defined(USE_HARDWARE_DCDC)
     if (OPT_USE_HARDWARE_DCDC)
     {
+        DBGLN("Enabling DCDC regulator");
         hal.WriteCommand(SX1280_RADIO_SET_REGULATORMODE, SX1280_USE_DCDC, SX12XX_Radio_All);        // Enable DCDC converter instead of LDO
     }
 #endif
